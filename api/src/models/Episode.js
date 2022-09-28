6aa6a66ed6ef237dc39df28a4b768adb7b876414
linker <diegolucas142@gmail.com>
2022-09-28 17:57:23 -0300

    models/ Episode

--- api/src/models/Episode.js
+++ api/src/models/Episode.js
@@ -1,9 +1,17 @@
-const { DataTypes } = require('sequelize')
+const { DataTypes, STRING } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('episode', {
-
+    id:{
+      type:DataTypes.INTEGER,
+      primaryKey: true
+    },
+    name:{
+      type:DataTypes.STRING,
+      allownull: false,
+      unique: true,
+    },
   })
 }
