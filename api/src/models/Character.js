5c18c42782f1a5db1e910271fdf2c17531bb9f34
linker <diegolucas142@gmail.com>
2022-09-28 17:53:29 -0300

    models

--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -4,6 +4,30 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("character", {
-
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allownull:false,
+    },
+    name:{
+      type: DataTypes.STRING,
+      allownull: false,
+    },
+    species:{
+      type: DataTypes.STRING,
+      allownull:false,
+    },
+    origin:{
+      type:DataTypes.STRING,
+      allownull:false,
+    },
+    image:{
+      type:DataTypes.STRING,
+    },
+    created:{
+      type:DataTypes.BOOLEAN,
+    },
+    
   });
 };
