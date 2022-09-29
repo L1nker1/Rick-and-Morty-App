d1cd86bb12cc1a13ccc9d2b34380bdce822c3e48
linker <diegolucas142@gmail.com>
2022-09-28 21:12:43 -0300

    modelos terminados y ruta que trae characters

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
+const {character} = require("./Characters")
+const {episode} = require("./Episodes")
 
 const router = Router();
 
 // Configurar los routers
+router.use("/character", character);
+router.use("/episode", episode)
 
 module.exports = router;
